95564ea6302099b3d4aaf6461883125e0bc2bb2e
vishal5394 <vishaljadyal@gmail.com>
2022-07-25 19:27:00 +0530

    Added: Welcome to Address Book Prgram

---
+++ addressbooks.java
@@ -0,0 +1,13 @@
+package com.BridgeLabz.AddressBook;
+
+public class addressbooks {
+
+     public static void main(String[] args) {
+	        System.out.println("Welcome to Address Book Program!");
+    	
+	}
+    }
+
+
+	     
+	    

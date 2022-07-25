6b49f34f6741a398ed4dec7d9a69b46ccfbae62a
vishal5394 <vishaljadyal@gmail.com>
2022-07-25 22:23:51 +0530

    Added: Edit Contact

--- addressbooks.java
+++ addressbooks.java
@@ -3,11 +3,13 @@ package com.BridgeLabz.AddressBook;
 import java.util.Scanner;
 
 public class addressbooks {
-	
+	 String firstName,lastName,address,city,state,zip,emailId;
+	 long phoneNumber;
+		Scanner scanner = new Scanner(System.in);
 	void askUser() {
-		Scanner scanner = new Scanner(System.in);	
 			System.out.println("Select an action..");
 			System.out.println("1. Add a person");
+			System.out.println("2. Edit");
 			System.out.println("2. Quit");
 			int choice = scanner.nextInt();	
 		switch(choice) {
@@ -19,13 +21,16 @@ public class addressbooks {
 			
 			default:
 			break;
+			
+		case 2:
+				edit();
+				System.out.println("\n");
+				askUser();
+				break;
 	
 		}
 	}
-	
-     	void addPerson() {
-	       
-	       String firstName,lastName,address,city,state,emailId;
+		void addPerson() {
 	       
 			Scanner sc=new Scanner(System.in);
 
@@ -48,10 +53,10 @@ public class addressbooks {
 	        emailId=sc.nextLine();
 	                  
 	        System.out.println("Enter zip code");
-	        int Zip=sc.nextInt();
+	         zip=sc.next();
 	        
 	        System.out.println("Enter phone number");
-	        long phoneNumber=sc.nextLong();
+	        phoneNumber=sc.nextLong();
 	         
 	        
 	      
@@ -62,6 +67,40 @@ public class addressbooks {
 	        System.out.println("Phone Number:"+ phoneNumber);
 	        System.out.println("Email Id:"+ emailId);
      	}
+     	
+     	void edit() {
+    		System.out.println("What do you want to edit?");
+    		System.out.println("1. Address");
+    		System.out.println("2. Phone Number");
+    		
+    		int choice = scanner.nextInt();
+    		switch (choice) {
+    		case 1:
+    			System.out.print("Enter address: ");
+    			scanner.nextLine();
+    			
+    			 address = scanner.nextLine();
+    			System.out.print("Enter city, state and zip: ");
+    			 city = scanner.next();
+    			state = scanner.next();
+    			 zip = scanner.next();
+    			break;
+
+    		case 2:
+    			System.out.println("Enter phone number: ");
+    			 phoneNumber = scanner.nextLong();
+    			
+    			break;
+    		}
+    		System.out.println("Contact updated successfully");
+    		
+    		System.out.println("Name:"+firstName+" "+lastName);
+ 	        System.out.println("Address:"+address);
+ 	        System.out.println("City:"+city);
+ 	        System.out.println("State:"+state);
+ 	        System.out.println("Phone Number:"+ phoneNumber);
+ 	        System.out.println("Email Id:"+ emailId);
+    	}
 	    
 	        public static void main(String[] args) {
 	        	

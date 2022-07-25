274ea66fb4048543cfc2ed43bda418b41f5c8d98
Vishal5394 <104050164+Vishal5394@users.noreply.github.com>
2022-07-25 22:51:42 +0530

    Update addressbooks.java

--- addressbooks.java
+++ addressbooks.java
@@ -10,7 +10,7 @@ public class addressbooks {
 			System.out.println("Select an action..");
 			System.out.println("1. Add a person");
 			System.out.println("2. Edit");
-			System.out.println("2. Quit");
+			System.out.println("3. Quit");
 			int choice = scanner.nextInt();	
 		switch(choice) {
 		case 1:
